ab947eb1261756f64bc083aace6c5b5d6a0de01c
[Joel] <[l221061095@mulege.tecnm.mx]>
2025-11-10 19:25:33 -0700

    config Modificación: Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,41 @@
 <?php
 
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+class Routes extends BaseConfig
+{
+    public function __construct()
+    {
+        $this->routes = new RouteCollection(null);
+        $this->configureRoutes();
+    }
+
+    private function configureRoutes(): void
+    {
+        // Página principal
+        $this->routes->get('/', 'Home::index');
+        
+        // Lecturas
+        $this->routes->get('lecturas/ultima', 'LecturasController::ultimaLectura');
+        $this->routes->get('lecturas/recientes', 'LecturasController::lecturasRecientes');
+        
+        // Lanzamientos
+        $this->routes->get('lanzamientos', 'LanzamientoController::index');
+        $this->routes->get('lanzamiento/ver/(:num)', 'LanzamientoController::ver/$1');
+        $this->routes->post('lanzamiento/iniciar', 'LanzamientoController::iniciar');
+        $this->routes->post('lanzamiento/finalizar', 'LanzamientoController::finalizar');
+        $this->routes->post('lanzamiento/finalizar/(:num)', 'LanzamientoController::finalizar/$1');
+        
+        // Exportación
+        $this->routes->get('export/pdf/(:num)', 'ExportController::pdfLanzamiento/$1');
+        $this->routes->post('export/multiples', 'ExportController::pdfMultiplesLanzamientos');
+    }
+
+    public function getRoutes(): RouteCollection
+    {
+        return $this->routes;
+    }
+}
